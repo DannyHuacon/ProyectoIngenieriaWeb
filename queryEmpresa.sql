9dac06f9e4381cd132d5cde870ddd94f16230044
DannyHuacon <55764753+DannyHuacon@users.noreply.github.com>
2022-10-06 11:50:48 -0500

    Add files via upload

---
+++ queryEmpresa.sql
@@ -0,0 +1,10 @@
+create database EmpresaWeb2
+use EmpresaWeb2
+go 
+
+CREATE TABLE EmpleadosWEB(
+	ID int IDENTITY(1,1) NOT NULL,
+	Nombres nvarchar(50) NULL,
+	Apellidos nvarchar (50) NULL,
+ CONSTRAINT PK_EmpleadosWEB PRIMARY KEY CLUSTERED 
+)
